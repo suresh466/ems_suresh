330b40c2eda1db063bfe8626bb0c62cbbfd732a0
Suresh <suresh.thagunna77@gmail.com>
2024-02-20 10:55:25 -0500

    db connection

---
+++ db.js
@@ -0,0 +1,15 @@
+const { MongoClient } = require('mongodb');
+
+let dbConnection;
+async function connectDb() {
+  const client = new MongoClient( "mongodb+srv://sthagunna6660-03:passwordadvfs@cluster3.ytit3gj.mongodb.net/?retryWrites=true&w=majority");
+  await client.connect();
+  console.log('Connected to MongoDB at', "mongodb+srv://sthagunna6660-03");
+  dbConnection = client.db();
+}
+
+function getDb() {
+  return dbConnection;
+}
+
+module.exports = { connectDb, getDb };
