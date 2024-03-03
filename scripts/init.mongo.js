ac89e1334172588dfbfced9170eb643530460083
Suresh <suresh.thagunna77@gmail.com>
2024-03-03 14:28:23 -0500

    Add employee data initialization script

---
+++ scripts/init.mongo.js
@@ -0,0 +1,30 @@
+/*
+ * Run using the mongo shell. For remote databases, ensure that the
+ * connection string is supplied in the command line. For example:
+ * localhost:
+ *   mongo issuetracker scripts/init.mongo.js
+ * Atlas:
+ *   mongo mongodb+srv://user:pwd@xxx.mongodb.net/issuetracker scripts/init.mongo.js
+ * MLab:
+ *   mongo mongodb://user:pwd@xxx.mlab.com:33533/issuetracker scripts/init.mongo.js
+ */
+
+db.employees.deleteMany({});
+
+const employeesDB = [
+    {
+        id: 1, firstName: 'Suresh', lastName: 'Thagunna', age: 25, dateOfJoining: new Date(), title: 'Manager',
+        department: 'IT', EmployeeType: "FullTime", currentStatus: "True",
+    },
+    {
+        id: 2, firstName: 'Deepak', lastName: 'Singh', age: 23, dateOfJoining: new Date(), title: 'VP',
+        department: 'IT', EmployeeType: "PartTime", currentStatus: "True",
+    },
+    ];
+
+db.employees.insertMany(employeesDB);
+const count = db.employees.countDocuments()
+print('Inserted', count, 'employees');
+
+db.counters.deleteOne({ _id: 'employees' });
+db.counters.insertOne({ _id: 'employees', current: count });
