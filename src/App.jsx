3a8d2eda7feec01c62bb2d711c7d0f7840dc54f5
Suresh <suresh.thagunna77@gmail.com>
2024-03-03 22:03:24 -0500

    Add style.css and refactor the structure of the html

--- src/App.jsx
+++ src/App.jsx
@@ -1,42 +1,42 @@
 const dateRegex = new RegExp('^\\d\\d\\d\\d-\\d\\d-\\d\\d');
 function jsonDateReviver(key, value) {
-  if (dateRegex.test(value)) return new Date(value);
-  return value;
+    if (dateRegex.test(value)) return new Date(value);
+    return value;
 }
 
 async function graphQLFetch(query, variables = {}) {
     try {
-      const response = await fetch('/graphql', {
-        method: 'POST',
-        headers: { 'Content-Type': 'application/json'},
-        body: JSON.stringify({ query, variables })
-      });
-      const body = await response.text();
-      const result = JSON.parse(body, jsonDateReviver);
-  
-      if (result.errors) {
-        const error = result.errors[0];
-        if (error.extensions.code == 'BAD_USER_INPUT') {
-          const details = error.extensions.errors.join('\n ');
-          alert(`${error.message}:\n ${details}`);
-        } else {
-          alert(`${error.extensions.code}: ${error.message}`);
+        const response = await fetch('/graphql', {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify({ query, variables })
+        });
+        const body = await response.text();
+        const result = JSON.parse(body, jsonDateReviver);
+
+        if (result.errors) {
+            const error = result.errors[0];
+            if (error.extensions.code == 'BAD_USER_INPUT') {
+                const details = error.extensions.errors.join('\n ');
+                alert(`${error.message}:\n ${details}`);
+            } else {
+                alert(`${error.extensions.code}: ${error.message}`);
+            }
         }
-      }
-      return result.data;
+        return result.data;
     } catch (e) {
-      alert(`Error in sending data to server: ${e.message}`);
+        alert(`Error in sending data to server: ${e.message}`);
     }
-  }
+}
 
 function EmployeeSearch() {
-  return (
-    <div>
-      <h2>Search:</h2>
-      <input type="text" name="search" id="search" placeholder="Search..." />
-      <input type="button" value="Search" />
-    </div>
-  );
+    return (
+        <section id="employee-search" className="employee-section">
+            <h2>Search:</h2>
+            <input type="text" name="search" id="search" placeholder="Search..." />
+            <input type="button" value="Search" />
+        </section>
+    );
 }
 
 function EmployeeRow(props) {
@@ -51,7 +51,7 @@ function EmployeeRow(props) {
             <td>{employee.title}</td>
             <td>{employee.department}</td>
             <td>{employee.employeeType}</td>
-            <td>{employee.currentStatus? '1' : '0'}</td>
+            <td>{employee.currentStatus ? '1' : '0'}</td>
         </tr>
     );
 }
@@ -71,45 +71,48 @@ class EmployeeCreate extends React.Component {
             title: form.title.value, department: form.department.value,
             employeeType: form.employeeType.value,
         }
-        
+
         this.props.createEmployee(employee);
         form.reset();
     }
-    
+
     render() {
         return (
-            <form name="employeeCreate" onSubmit={this.handleSubmit}>
-                <label htmlFor="firstName">First Name:</label>
-                <input type="text" id="firstName" name="firstName" />
-                <label htmlFor="lastName">Last Name:</label>
-                <input type="text" id="lastName" name="lastName" />
-                <label htmlFor="age">Age:</label>
-                <input type="text" id="age" name="age" />
-                <label htmlFor="dateOfJoining">Date Of Joining:</label>
-                <input type="date" id="dateOfJoining" name="dateOfJoining" />
-                <label htmlFor="title">Title:</label>
-                <select name="title" id="title">
-                    <option value="Employee">Employee</option>
-                    <option value="Manager">Manager</option>
-                    <option value="Director">Director</option>
-                    <option value="VP">VP</option>
-                </select>
-                <label htmlFor="department">Department:</label>
-                <select name="department" id="department">
-                    <option value="IT">IT</option>
-                    <option value="HR">HR</option>
-                    <option value="Marketing">Marketing</option>
-                    <option value="Engineering">Engineering</option>
-                </select>
-                <label htmlFor="employeeType">employeeType:</label>
-                <select name="employeeType" id="employeeType">
-                    <option value="FullTime">FullTime</option>
-                    <option value="PartTime">PartTime</option>
-                    <option value="Contract">Contract</option>
-                    <option value="Seasonal">Seasonal</option>
-                </select>
-                <input type="submit" value="Add" />
-            </form>
+            <section id="employee-create" className="employee-section">
+                <h2>Create Employee:</h2>
+                <form name="employeeCreate" onSubmit={this.handleSubmit}>
+                    <label htmlFor="firstName">First Name:</label>
+                    <input type="text" id="firstName" name="firstName" />
+                    <label htmlFor="lastName">Last Name:</label>
+                    <input type="text" id="lastName" name="lastName" />
+                    <label htmlFor="age">Age:</label>
+                    <input type="text" id="age" name="age" />
+                    <label htmlFor="dateOfJoining">Date Of Joining:</label>
+                    <input type="date" id="dateOfJoining" name="dateOfJoining" />
+                    <label htmlFor="title">Title:</label>
+                    <select name="title" id="title">
+                        <option value="Employee">Employee</option>
+                        <option value="Manager">Manager</option>
+                        <option value="Director">Director</option>
+                        <option value="VP">VP</option>
+                    </select>
+                    <label htmlFor="department">Department:</label>
+                    <select name="department" id="department">
+                        <option value="IT">IT</option>
+                        <option value="HR">HR</option>
+                        <option value="Marketing">Marketing</option>
+                        <option value="Engineering">Engineering</option>
+                    </select>
+                    <label htmlFor="employeeType">employeeType:</label>
+                    <select name="employeeType" id="employeeType">
+                        <option value="FullTime">FullTime</option>
+                        <option value="PartTime">PartTime</option>
+                        <option value="Contract">Contract</option>
+                        <option value="Seasonal">Seasonal</option>
+                    </select>
+                    <input type="submit" value="Add" />
+                </form>
+            </section>
         );
     }
 }
@@ -118,23 +121,25 @@ function EmployeeTable(props) {
     const employeeRows = props.employees.map(employee => <EmployeeRow key={employee.id} employee={employee} />);
 
     return (
-        <table>
-            <thead>
-                <tr>
-                    <th>First Name</th>
-                    <th>Last Name</th>
-                    <th>Age</th>
-                    <th>Date Of Joining</th>
-                    <th>Title</th>
-                    <th>Department</th>
-                    <th>Employee Type</th>
-                    <th>Current Status</th>
-                </tr>
-            </thead>
-            <tbody>
-                { employeeRows }
-            </tbody>
-        </table>
+        <section id="employee-table" className="employee-section">
+            <table>
+                <thead>
+                    <tr>
+                        <th>First Name</th>
+                        <th>Last Name</th>
+                        <th>Age</th>
+                        <th>Date Of Joining</th>
+                        <th>Title</th>
+                        <th>Department</th>
+                        <th>Employee Type</th>
+                        <th>Current Status</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {employeeRows}
+                </tbody>
+            </table>
+        </section>
     );
 }
 
@@ -155,35 +160,35 @@ class EmployeeDirectory extends React.Component {
                 id firstName lastName age dateOfJoining title department employeeType currentStatus
             }
         }`;
-        
+
         const data = await graphQLFetch(query);
         if (data) this.setState({ employees: data.employeeList });
     }
-    
+
     async createEmployee(employee) {
         const query = `mutation employeeCreate($employee: EmployeeInputs!) {
             employeeCreate(employee: $employee) {
                 id
             }
         }`;
-        
+
         const data = await graphQLFetch(query, { employee });
         if (data) this.loadData();
     }
 
-  render() {
-    return (
-        <React.Fragment>
-      <div>
-        <h1>Employee Directory</h1>
-      </div>
-        <EmployeeSearch />
-        <hr />
-        <EmployeeTable employees = {this.state.employees} />
-        <EmployeeCreate createEmployee = {this.createEmployee} />
-      </React.Fragment>
-    );
-  }
+    render() {
+        return (
+            <main id="employee-directory">
+                <div>
+                    <h1>Employee Management System</h1>
+                </div>
+                <EmployeeSearch />
+                <hr />
+                <EmployeeTable employees={this.state.employees} />
+                <EmployeeCreate createEmployee={this.createEmployee} />
+            </main>
+        );
+    }
 }
 
 ReactDOM.render(<EmployeeDirectory />, document.getElementById('content'));
